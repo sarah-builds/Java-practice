ca453aa1005e9dd2971701de29b535feb122e8ca
sarah-builds <ansarisarah463@gmail.com>
2026-05-17 20:48:57 +0530

    Create loop.java

---
+++ loop.java
@@ -0,0 +1,50 @@
+public class CW_loop {
+    public static void main(String[] args) {
+       int a=0;
+       do {
+           System.out.println(a);
+           a++;
+       }while (a<=5);
+
+       int b=0;
+       while (b<=5){
+           System.out.println(b);
+           b++;
+       }
+int n=5;
+       for (int i=0;i<=n;i++){
+           System.out.println(2*i+1);
+       }
+       for (int i=5;i>0;i--){
+           System.out.println(i);
+       }
+
+       break & continue
+       for (int i=0;i<=5;i++){
+           System.out.println(i);
+           if (i==2){
+               System.out.println("ends here");
+               break;
+           }
+       }
+       i++;
+           if (i==2){
+               System.out.println("ends here");
+               break;
+           }
+       }while (i<=5);
+
+        for (int i=0;i<=5;i++) {
+            if (i == 2) {
+                continue;
+            }
+            System.out.println(i);
+        }
+
+
+    }
+}
+
+//        int i=0;
+//        do {
+//            System.out.println(i);
